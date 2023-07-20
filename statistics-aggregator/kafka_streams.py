d2306ceee8b77d907f35151ff666ea6a0f0397ae
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-07-20 23:07:43 +0300

    Added sentiments table

--- statistics-aggregator/kafka_streams.py
+++ statistics-aggregator/kafka_streams.py
@@ -1,6 +1,7 @@
 import json
 import faust
 import logging
+import time
 
 from logger import CustomHandler
 from config import *
@@ -19,17 +20,32 @@ app = faust.App('statistics-aggregator',
                 value_serializer='raw',
                 web_host=FAUST_HOST,
                 web_port=FAUST_PORT)
-languages_topic = app.topic(LANGUAGES_TOPIC)
+languages_topic = app.topic(LANGUAGES_TOPIC, partitions=3)
 langs_table = app.Table("langs-count", key_type=str, value_type=int, partitions=3, default=int)
+
+sentiments_topic = app.topic(SENTIMENTS_TOPIC, partitions=3)
+sentiments_table = app.Table("sentiments-count", key_type=str, value_type=int, partitions=3, default=int)
 
 
 @app.agent(languages_topic)
-async def process_languages(records):
+async def process_languages(languages):
     i = 0
-    async for record in records:
+    async for record in languages:
         # Get message details
         message = json.loads(record)
         langs_table[message['language']] += 1
         i += 1
         if i % 10 == 0:
             print(langs_table.as_ansitable(title='Languages Table'))
+
+
+@app.agent(sentiments_topic)
+async def process_sentiments(sentiments):
+    i = 0
+    async for record in sentiments:
+        # Get message details
+        message = json.loads(record)
+        sentiments_table[message['sentiment']] += 1
+        i += 1
+        if i % 10 == 0:
+            print(sentiments_table.as_ansitable(title='Sentiments Table'))
